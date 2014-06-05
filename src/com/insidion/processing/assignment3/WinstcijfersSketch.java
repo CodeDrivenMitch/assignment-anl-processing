d2688d392e3678ad74430bc8349b24b5f061278b
Mitchell Herrijgers <mitchellherrijgers@gmail.com>
2014-06-05 13:22:24 +0200

    Colors, rotated labels and Legenda Space

--- src/com/insidion/processing/assignment3/WinstcijfersSketch.java
+++ src/com/insidion/processing/assignment3/WinstcijfersSketch.java
@@ -13,6 +13,9 @@ public class WinstcijfersSketch extends Assignment {
     private static final int YEAR_SEP_HEIGHT = 5;
     private static final int VALUE_SEP_WIDTH = 5;
     private static final int VALUE_SCALE = 25;
+    private static final int LEGENDA_WIDTH = 100;
+
+    private int[] colors;
     Table table;
     private int maxValue;
     private int length;
@@ -24,16 +27,32 @@ public class WinstcijfersSketch extends Assignment {
         table = loadTable("http://web.insidion.com/processing/o3/Winstcijfers.tsv", "header, tsv");
         maxValue = getMaxValue();
         length = getDataLength();
+        initColors();
     }
 
     @Override
     public void draw() {
         fill(0, 0, 0);
         clear();
+
+        drawData();
         drawFrames();
         drawHorLabels();
         drawVertLabels();
-        drawData();
+
+
+    }
+
+    private void initColors() {
+        this.colors = new int[10];
+
+        this.colors[0] = color(255, 0, 0);
+        this.colors[1] = color(0, 255, 0);
+        this.colors[2] = color(0, 0, 255);
+        this.colors[3] = color(255, 0, 255);
+        this.colors[4] = color(255, 255, 0);
+        this.colors[5] = color(0, 255, 255);
+
     }
 
 
@@ -42,29 +61,34 @@ public class WinstcijfersSketch extends Assignment {
         fill(255, 255, 255);
 
         line(MARGIN, MARGIN, MARGIN, height - MARGIN);
-        line(MARGIN, height - MARGIN, width - MARGIN, height - MARGIN);
+        line(MARGIN, height - MARGIN, width - MARGIN - LEGENDA_WIDTH - ((width - 2 * MARGIN) / table.getRowCount()), height - MARGIN);
     }
 
     private void drawHorLabels() {
-        int space = width - 2 * MARGIN;
+        int space = width - 2 * MARGIN - LEGENDA_WIDTH;
         float spacePerRow = (float) space / length;
 
         for (int i = 0; i < length; i++) {
-            int lineX = (int) (MARGIN + spacePerRow * i);
+            int lineX = round(MARGIN + spacePerRow * i);
             int lineY = height - MARGIN;
             line(lineX, lineY, lineX, lineY - YEAR_SEP_HEIGHT);
 
             float labelX = (MARGIN + (spacePerRow * i));
             float labelY = height - MARGIN + 10;
             textAlign(LEFT);
-            text(table.getRow(i).getString(0), labelX, labelY);
+            pushMatrix();
+            translate(labelX, labelY);
+            rotate(radians(45));
+            text(table.getRow(i).getString(0), 0, 0);
+            popMatrix();
+
 
         }
     }
 
     private void drawVertLabels() {
         int length = maxValue / VALUE_SCALE;
-        int space = height - 2 * MARGIN;
+        int space = height - 2 * MARGIN - LEGENDA_WIDTH;
         int spacePerThing = space / length;
 
         for (int i = 1; i <= length; i++) {
@@ -99,15 +123,17 @@ public class WinstcijfersSketch extends Assignment {
     }
 
     private void drawData() {
-        for(int i = 1; i < table.getRowCount(); i++) {
-            for(int j = 1; j < table.getColumnCount(); j++) {
-                stroke(255, 255, 255);
-                fill(255,255,255);
-                float xLineBegin = MARGIN + (((width - 2 * MARGIN)/length) * (i - 1));
-                float xLineEnd = MARGIN + ((width - 2 * MARGIN)/length) * (i) ;
+        strokeWeight(4);
+        for (int i = 1; i < table.getRowCount(); i++) {
+            for (int j = 1; j < table.getColumnCount(); j++) {
+                stroke(this.colors[j]);
+                fill(this.colors[j]);
+                float xLineBegin = MARGIN + (((width - 2 * MARGIN - LEGENDA_WIDTH) / length) * (i - 1));
+                float xLineEnd = MARGIN + ((width - 2 * MARGIN - LEGENDA_WIDTH) / length) * (i);
                 float yLineBegin = map(table.getRow(i - 1).getInt(j), 0, maxValue, height - MARGIN, MARGIN);
-                float yLineEnd =  map(table.getRow(i).getInt(j), 0, maxValue, height - MARGIN, MARGIN);
+                float yLineEnd = map(table.getRow(i).getInt(j), 0, maxValue, height - MARGIN, MARGIN);
                 line(xLineBegin, yLineBegin, xLineEnd, yLineEnd);
+                ellipse(xLineEnd, yLineEnd, 3, 3);
             }
         }
 
