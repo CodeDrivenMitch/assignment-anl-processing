705ce5df2a0be2e8f607c00fb6fad6e1786a08a9
husseinhoussein <houssein.hussein@gmail.com>
2014-06-13 20:04:09 +0200

    I DID A THING!!!
    ...... well it does look like water xD

--- src/com/insidion/processing/assignment6/HeightMap.java
+++ src/com/insidion/processing/assignment6/HeightMap.java
@@ -4,6 +4,7 @@ import com.insidion.processing.framework.Assignment;
 import processing.data.Table;
 import processing.data.TableRow;
 
+
 /**
  * Created by mitchell on 6/13/2014.
  */
@@ -18,20 +19,24 @@ public class HeightMap extends Assignment {
     public void setup() {
         super.setup();
         resize(1000, 1000);
+        colorMode(RGB);
 
         table = loadTable("http://web.insidion.com/processing/o6/processed_500.csv");
     }
 
     @Override
     public void draw() {
-        int black = color(0);
+
+        //int black = color(1);
+       float blue1 = color(10, 0, 0,255);
+        fill(blue1);
         for(int i = 0; i < table.getRowCount(); i++) {
             TableRow row = table.getRow(i);
             int x = row.getInt(0) - (center_x - distance);
             int y = row.getInt(1) - (center_y - distance);
 
-            System.out.println("X:" + x + ", Y:" + y);
-            set(x, y, black);
+            //System.out.println("X:" + x + ", Y:" + y);
+             set(x, y,  i*1000000000*20);
         }
 
 
@@ -41,6 +46,6 @@ public class HeightMap extends Assignment {
 
     @Override
     public String toString() {
-        return "Assignment 6 - Height Map";
+        return "Assignment Six - Height Map";
     }
 }
