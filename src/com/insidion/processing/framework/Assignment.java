19fda9b15bf41cd343e52d575e24046116392e3e
Mitchell Herrijgers <mitchellherrijgers@gmail.com>
2014-06-15 11:18:18 +0200

    Refined Assignment class

--- src/com/insidion/processing/framework/Assignment.java
+++ src/com/insidion/processing/framework/Assignment.java
@@ -3,40 +3,45 @@ package com.insidion.processing.framework;
 import processing.core.PApplet;
 
 /**
- * Created by mitchell on 6/5/2014.
+ * Base class for all assignments. Sets up some basic colors to be used, a launch method for use in the Launcher,
+ * and defaults sizes and title.
  */
 public abstract class Assignment extends PApplet {
-    protected int[] colors;
+    protected int[] colors = new int[]{
+            color(255, 0, 0),
+            color(0, 255, 0),
+            color(0, 0, 255),
+            color(255, 0, 255),
+            color(255, 255, 0),
+            color(0, 255, 255),
+    };
+
     /**
      * Force assignments to implement a toString method for the list text in the launcher
+     *
      * @return Name of assignment
      */
     public abstract String toString();
 
+    /**
+     * Setup functions. Implementing subclasses can call this using super.setup(), but they can also override
+     * the method for more specialized use.
+     */
     @Override
     public void setup() {
+        // Set the default size. For some reason, it bugs without using the resize function also.
         size(800, 500);
-        resize(800,500);
+        resize(800, 500);
+
+        // Set the frame title
         frame.setTitle(this.toString());
-        initColors();
     }
+
     /**
      * Make Assignment applets Launchable
      */
     public void launch() {
         PApplet.main(this.getClass().getName());
-
-    }
-
-    private void initColors() {
-        this.colors = new int[10];
-
-        this.colors[0] = color(255, 0, 0);
-        this.colors[1] = color(0, 255, 0);
-        this.colors[2] = color(0, 0, 255);
-        this.colors[3] = color(255, 0, 255);
-        this.colors[4] = color(255, 255, 0);
-        this.colors[5] = color(0, 255, 255);
 
     }
 
