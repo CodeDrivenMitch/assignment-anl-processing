60b64ca684c9d2102369377ba82a9620f63a5d57
Mitchell Herrijgers <mitchellherrijgers@gmail.com>
2014-06-19 12:52:29 +0200

    Tada

--- src/com/insidion/processing/assignment2/VolcanicSketch.java
+++ src/com/insidion/processing/assignment2/VolcanicSketch.java
@@ -20,6 +20,9 @@ public class VolcanicSketch extends Assignment {
     Point upperleft = new Point(102, 151);
     Point downright = new Point(790, 370);
 
+    /**
+     * Sets up the sketch
+     */
     public void setup() {
         super.setup();
         colorMode(HSB, 360, 100, 100);
@@ -33,6 +36,9 @@ public class VolcanicSketch extends Assignment {
 
     }
 
+    /**
+     * Draws the sketch
+     */
     public void draw() {
         img.resize(width, height);
         image(img, 0, 0);
@@ -57,9 +63,12 @@ public class VolcanicSketch extends Assignment {
     }
 
 
+    /**
+     * Loads the data from the webserver
+     */
     private void loadData() {
          //dataset word geladen, latitude en longtitude tabellen worden duidelijk en apart opgeroepen.
-        table = loadTable("http://web.insidion.com/data.csv", "header");
+        table = loadTable("http://web.insidion.com/processing/o2/data.csv", "header");
         table.addColumn("lat");
         table.addColumn("lng");
          // forloop gaat alle rijen af en roept de waardes op.
@@ -75,20 +84,38 @@ public class VolcanicSketch extends Assignment {
 
     }
 
+    /**
+     * Draws the legend.
+     */
     private void drawLegend() {
         // Draw the legend text
         stroke(0, 0, 0);
+        fill(color(255, 0, 255));
+        rect(width, height, -260, -150);
+
+        // Draw the legend text
         fill(0, 0, 0);
         textAlign(RIGHT);
-        text("Weak quake", width - 35, height - 120);
-        text("Strong quake", width - 35, height - 10);
+        text("Shorter quake", width - 35, height - 120);
+        text("Longer quake", width - 35, height - 10);
+        text("Stronger quake", width - 165, height - 115);
+        text("Weaker quake", width - 165, height - 15);
 
-        // Draw the legend shape. Uses a self-made gradient
+        // Draw the legend shape of length. Uses a self-made gradient
         rect(width - 30, height - 130, 20, 121);
         for (int i = 0; i < 120; i++) {
             stroke(100, (float) 1.8 * i / 2, 100);
             line(width - 29, height - 129 + i, width - 11, height - 129 + i);
         }
+
+        // Draw the legend shape of quake strength
+        stroke(0, 0, 0);
+        fill(color(255, 0, 255));
+        ellipse(width - 140, height - 120, 10, 10);
+        ellipse(width - 140, height - 90, 15, 15);
+        ellipse(width - 140, height - 60, 20, 20);
+        ellipse(width - 140, height - 20, 30, 30);
+
 
     }
 
